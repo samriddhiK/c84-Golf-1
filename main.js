27427a50b84ebed9fa91e15375c8ae804762bc88
samriddhiK <75959569+samriddhiK@users.noreply.github.com>
2023-03-12 17:16:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,81 @@
-// Create a reference for the canvas
 
-img_width = 300;
-img_height = 100;
+// Create canvas variable
 
-var img_image;
+//Set initial positions for ball and hole images.
 
-img_x = 100;
-img_y = 100;
 
-function add() {
-	img_imgTag = new Image(); //defining a variable with a new image
-	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
-	img_imgTag.src = img_image;   // load image
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img(){
+	// write code to Upload golf image on the canvas
+	new_image();
 }
 
-function uploadimg() {
-
-	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+function new_image()
+{
+	// write code to Upload ball image on canvas
 }
 
-//Write a code to grab the key-pressed event
+window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
+	/* Check the coordinates of the ball and hole images to finish the game. 
+	And id coordinates matches them remove ball image, 
+	display "GAME OVER!!!" 
+	and make canvas border 'red'. */
 	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
-		//write a code to check the type of key pressed
 	else{
-		otherkey();
-		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
 	}
+	
+	function up()
+	{
+		// Write a code to move ball upward.
+	}
+
+	function down()
+	{
+		 // Write a code to move ball downward.
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			// Write a code to move ball left side.
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			// Write a code to move ball right side.
+		}
+	}
+	
 }
 
-function alphabetkey()
-{
-	//upload respective image with the message. 
-
-}
-function numberkey()
-{
-	
-}
-function arrowkey()
-{
-}
-function specialkey()
-{
-	
-}
-function otherkey()
-{
-	img_image="otherkey.png";
-	add();
-}
-	
